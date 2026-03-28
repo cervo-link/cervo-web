e95d3d26e13fb9e015ab10a4d45d67cfeae931ae
João Pedro Alves <joaopealves.dev@gmail.com>
2026-03-27 21:37:29 -0300

    chore: update biome configuration by removing unused content-collections and integrating TanStack Query for improved data management

---
+++ src/integrations/tanstack-query/devtools.tsx
@@ -0,0 +1,8 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const TanStackQueryDevtools = {
+	name: 'TanStack Query',
+	render: <ReactQueryDevtools />,
+}
+
+export default TanStackQueryDevtools
